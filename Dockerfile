cf3fe668311d70bfa2b7aa31ab6cd08019735059
Yoav Elhadad <yoave14@gmail.com>
2022-10-19 23:06:28 +0300

    now the files

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:latest
+ENV VERSION = 1.2.0
+RUN apt update
+RUN apt install -y python3 vim zip unzip
+RUN mkdir text_files && mkdir zip_files
+COPY zip_job.py /tmp
+CMD cat /etc/os-release; cat /tmp/zip_job.py
---
+++ zip_job.py
@@ -0,0 +1,35 @@
+import sys, zipfile, os, fnmatch
+
+VERSION = os.environ["VERSION"]
+
+
+"""return true if there are 4 files in the provided directory and format
+    else return false"""
+def check_if_4_files(directory_path, file_format):
+    count = len(fnmatch.filter(os.listdir(directory_path), f'*.{file_format}'))
+    return count == 4
+
+def main():
+
+    arr = ['a', 'b', 'c', 'd']
+    text_files_path = './text_files'
+    zip_files_path = './zip_files'
+
+    # make sure the script fails if this function doesnt work
+    for x in arr:
+        f = open(f'{text_files_path}/{x}.txt', 'w')
+        f.write(x)
+        f.close()
+    if check_if_4_files(text_files_path,'txt') is False:
+        sys.exit('not all txt files created')
+
+    # fail the script if this step doesnt work
+    for x in arr:
+        zipfile_name = f'{zip_files_path}/{x}_{VERSION}.zip'
+        zipfile.ZipFile(zipfile_name, mode='w').write(f'{text_files_path}/{x}.txt')
+    if check_if_4_files(zip_files_path, 'zip') is False:
+        sys.exit('not all zip files created')
+
+
+if __name__ == '__main__':
+    main()
